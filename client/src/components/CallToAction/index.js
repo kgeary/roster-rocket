8c55091ffa5f43588a2ed96d667a5144958ede7f
Patrick Duquette <duquetteman@gmail.com>
2020-03-04 09:58:32 -0600

    Fixed the call to action button so that it jumps to the top of the window.

--- client/src/components/CallToAction/index.js
+++ client/src/components/CallToAction/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
+
 const CallToAction = () => {
   return (
     <div className='container-fluid call-to-action'>
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SignupForm from "../components/forms/SignupForm";
 import Parallax from "../components/Parallax";
 import CallToAction from "../components/CallToAction";
 
 function Signup() {
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }, []);
+
   return (
     <div>
       <SignupForm />
