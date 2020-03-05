4a1f9c9e48ffd33fb69e9b01c42c2da2cd2e4bab
Patrick Duquette <duquetteman@gmail.com>
2020-03-05 10:50:53 -0600

    Working on class roster CSV export function.

---
+++ client/src/components/ClassCSV/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { ExportToCsv } from 'export-to-csv';
+
+
+
+csvExporter.generateCsv(data){
+
+const csvExporter = new ExportToCsv(data);
+const data = <CardCourse />;
+console.log(data);
+};
+
+
+// Messing around with some of the example code from https://www.npmjs.com/package/export-to-csv}
+// npm install --save export-to-csv
+// Patrick
